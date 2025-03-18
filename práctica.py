d86c241370218acbaecf57a29ff599a5f3c36e64
KokoMoraDesigns <yo.kokomora@gmail.com>
2025-03-18 20:28:21 +0100

    Add files via upload

---
+++ práctica.py
@@ -0,0 +1,62 @@
+# EXERCISE 1
+
+string = 'good morning, my love'
+number = 5
+list = [ 1, 2, 3, 4, 5]
+boolean = True
+
+
+# EXERCISE 2
+
+exercise_two = string[0:3]
+
+
+
+# EXERCISE 3
+
+exercise_three = list[0]
+
+
+# EXERCISE 4
+
+number += 10
+
+
+# EXERCISE 5
+
+exercise_five = list[-1]
+
+
+# EXERCISE 6
+
+names = 'harry,alex,susie,jared,gail,conner'
+
+names_list = names.split(',')
+
+# EXERCISE 7
+
+first_word = string[0:4]
+
+first_word_upper = first_word.upper()
+
+new_string = first_word_upper + string[4:]
+
+
+# EXERCISE 8
+
+string_interpolation = f'{number} birds are singing in my window'
+
+
+# EXERCISE 9
+
+print('hello world')
+
+
+# EXERCISE 10
+
+
+cadena = 'hola, hoy es un día precioso'
+
+buscar = cadena.find('hola')
+
+reemplazar = cadena.replace('hola', 'adiós')
